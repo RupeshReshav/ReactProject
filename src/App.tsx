df9c8c94471a2889d7a70182abe8d634e640d9e8
Rupesh Sah <v-rusah@microsoft.com>
2019-06-24 19:18:57 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.scss';
+import  Box from './Component/Box/Box';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+     <main>
+       <Box/>
+     </main>
+       
+    );
+  }
+}
+
+export default App;
